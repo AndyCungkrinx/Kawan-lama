6b019b1d3cdca8f9577821dab2de27ef30c1fe94
AndyCungkrinx <57337592+AndyCungkrinx@users.noreply.github.com>
2020-05-03 22:00:31 +0700

    Create install magento

---
+++ install-magento.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#variable default
+server_name="http://local.magento2.test"
+db-host="127.0.0.1"
+db-name="m2_webapp"
+db-user="icube"
+db-pass="Icube123!"
+mg-pass="kawanlama123!"
+
+
+echo "=============================================
+-------------- Install Magento --------------
+============================================="
+cd /var/public/releases/
+git clone https://github.com/AndyCungkrinx/magento2.git
+cd magento2
+git checkout 2.2
+chmod u+x bin/magento
+composer install
+ln -s /var/public/releases /var/public/current
+chown icube:icube /var/public/ -R
+
+#magento config
+bin/magento setup:install \
+ --cleanup-database \
+ --base-url=$server_name \
+ --db-host=$db-host \
+ --db-name=$db-name \
+ --db-user=$db-user \
+ --db-password=$db-pass \
+ --backend-frontname=klbackoffice \
+ --admin-firstname=Kawanlama\
+ --admin-lastname=Administrator \
+ --admin-email=andy@icube.us \
+ --admin-user=admin \
+ --admin-password=$mg-pass \
+ --language=id_ID \
+ --currency=IDR \
+ --timezone=Asia/Bangkok \
+ --use-rewrites=1
+chown icube:icube /var/public/ -R
+chmod 777 var/ pub/ generated/ -R
